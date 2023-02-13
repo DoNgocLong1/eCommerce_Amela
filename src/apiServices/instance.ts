eebacb45ceb54bd4175311377c460b3c97ce4471
Do Ngoc Long <dongoclong101001@gmail.com>
2023-02-13 22:29:35 +0700

    change API

--- src/apiServices/instance.ts
+++ src/apiServices/instance.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = "http://34.87.154.7/api";
+const url = "http://34.142.255.109/api";
 export const instance = axios.create({
   baseURL: url,
   timeout: 1000,
